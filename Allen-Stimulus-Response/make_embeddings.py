022d30d266023e5da94c97f1d8b982a7afbeff5a
Maria <mariarosekesa@gmail.com>
2023-12-16 01:18:17 +0200

    CLIP works

--- Allen-Stimulus-Response/make_embeddings.py
+++ Allen-Stimulus-Response/make_embeddings.py
@@ -3,16 +3,17 @@ from pathlib import Path
 from config import cache_path, save_path, embeddings_dct
 import numpy as np 
 import torch
-from transformers import CLIPProcessor, CLIPModel, ViTImageProcessor, ViTModel
+from transformers import CLIPVisionModel, ViTImageProcessor, ViTModel, AutoProcessor
 import os
 
 
 class StimPrep:
     def __init__(self):
-        self.movies_id='501704220' #This is three session A
-        self.natural_scenes_id='501559087'
+        self.movies_id=501704220 #This is three session A
+        self.natural_scenes_id=501559087
+        print(cache_path)
         self.boc = BrainObservatoryCache(manifest_file=str(
-            cache_path / Path('brain_observatory_manifest.json')))
+            Path(cache_path) / Path('brain_observatory_manifest.json')))
         '''
         [{'id': 501559087, 'imaging_depth': 175, 'targeted_structure': 'VISp', 'cre_line': 'Cux2-CreERT2', 
         'reporter_line': 'Ai93(TITL-GCaMP6f)', 'acquisition_age_days': 103, 'experiment_container_id': 511510736, 
@@ -30,8 +31,8 @@ class StimPrep:
         '''
         
     def load_data(self, raw_stim):
-        stim_path = save_path/Path(raw_stim)
-        stimulus=stim_path.split('.')[0]
+        stim_path = Path(save_path)/Path(raw_stim)
+        stimulus=raw_stim.split('.')[0]
         if os.path.exists(stim_path):
             stims=np.load(stim_path)
             return stims
@@ -56,10 +57,11 @@ class StimPrep:
             inputs = processor(images=stims[i], return_tensors="pt")
             with torch.no_grad():
                 outputs = model(**inputs)
-            # print(outputs.keys())
-            last_hidden_states = outputs.pooler_output.squeeze(0).detach().numpy()
-            print(last_hidden_states.shape)
-            embeddings[i, :] = last_hidden_states
+            #last_hidden_states = outputs.last_hidden_state.mean(dim=1).squeeze().detach().numpy()
+            cls=outputs.pooler_output.squeeze().detach().numpy()
+            #print(cls.shape)
+            embeddings[i, :] = cls
+        return embeddings
         
     def make_dino(self):
         stims = np.repeat(stims[:, np.newaxis, :, :], 3, axis=1)
@@ -68,14 +70,14 @@ class StimPrep:
 
     def make_clip(self,stims):
         stims = np.repeat(stims[:, np.newaxis, :, :], 3, axis=1)
-        processor = CLIPProcessor.from_pretrained('facebook/dino-vitb8')
-        model = CLIPModel.from_pretrained('facebook/dino-vitb8')
+        model = CLIPVisionModel.from_pretrained("openai/clip-vit-base-patch32")
+        processor = AutoProcessor.from_pretrained("openai/clip-vit-base-patch32")
         embeddings  = self.process_stims(stims, processor, model)
         return embeddings
 
     def make_embedding(self, emb_path, stim_path, model):
         #Make CLIP
-        full_path = save_path / Path(emb_path)
+        full_path = Path(save_path) / Path(emb_path)
         if os.path.exists(full_path):
             print(f'{full_path} already exists!')
         else:
--- Allen-Stimulus-Response/pipeline.py
+++ Allen-Stimulus-Response/pipeline.py
@@ -13,11 +13,13 @@ class Pipeline:
 
     def run_pipeline(self):
         #Check if stimulus embeddings exist, start with CLIP
-        for p in embeddings_dct['clip']:
-            stim_path = save_path / Path(p)
-            if os.path.exists(stim_path):
+        for i, p in enumerate(embeddings_dct['clip']):
+            emb_path = save_path / Path(p)
+            raw_stim = embeddings_dct['raw_stims'][i]
+            if os.path.exists(emb_path):
                 print(f'CLIP {p} already exists!')
             else:
-                stim_prep.make_embedding(stim_path, model='CLIP')
+                self.stim_prep.make_embedding(emb_path, raw_stim, model='CLIP')
         
+Pipeline().run_pipeline()
 
