69ae772438d81ed3b4ae0db0843ebd42296f64f2
Maria <mariarosekesa@gmail.com>
2024-01-06 00:18:26 +0200

    embeddings

---
+++ Allen-Stimulus-Response/figure_1_data.py
@@ -0,0 +1,29 @@
+import numpy as np
+from config import save_path,stimuli_dct
+from pathlib import Path
+import matplotlib.pyplot as plt
+import json
+
+def make_embedding_json(movie):
+    paths=stimuli_dct[movie]
+    path=Path(save_path)/Path(paths['clip'])
+    my_dat=np.load(path)
+    #for i in range(0,my_dat.shape[0]-1):
+        #plt.plot(my_dat[i,:]-my_dat[i+1])
+        #plt.show()
+    previous_step_diff = []
+    #for i in range(1, my_dat.shape[0]):
+        #plt.scatter(my_dat[i-1,:],my_dat[i])
+        #plt.show()
+    json_dct={}
+    for i in range(1,my_dat.shape[0]):
+        json_dct[i]=list(my_dat[i-1]-my_dat[i])
+    json_path=Path(save_path)/Path("clip_emb_test.json")
+    with open(json_path, "w") as outfile: 
+        json.dump(json_dct, outfile, indent=4)
+
+
+make_embedding_json('movie_one')
+
+
+
